b6983983f53bd8734d8dcaba3411b3dd6e2769b2
felix <vsi_felixyao@126.com>
2019-01-10 10:46:28 +0800

    增加ViewToSource 方法使之能对 对象源赋值

--- unit/mysql/config.go
+++ unit/mysql/config.go
@@ -2,8 +2,8 @@ package mysql
 //数据库配置
 const (
 	userName = "root"
-	password = "mysql@izhaohu"
-	ip = "test.icloudcare.com"
+	password = "root"
+	ip = "172.0.0.1"
 	port = "3306"
 	dbName = "izhaohu"
 )
